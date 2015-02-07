822cefdca490ee292d42ab5812454f446e53232e
Jack Fischer <jfische8@binghamton.edu>
2015-02-07 12:34:35 -0500

    final set up, ready to start logging

--- laundry.rb
+++ laundry.rb
@@ -1,3 +1,4 @@
+
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
@@ -6,35 +7,28 @@ require 'csv'
 #buildingID = ARGV[0]
 
 
-
 CSV.open("log.csv", "ab")
-buildings = { "Mohawk" => "1008720" }
+buildings = { "BroomeDryers" => "1040623",
+              "BroomeWashers" => "1040603",
+              "EndicottDryers" => "1040646",
+              "EndicottWashers" => "1040624" }
 
-dryerAvail = 0
-washerAvail = 0
 buildings.each do |building, id|
-    url = "http://binghamton-asi.esuds.net/RoomStatus/machineStatus.i?bottomLocationId=#{id}"
-    page = Nokogiri::HTML(open(url))
-    machines = page.css('font')
+   machinesAvail = 0
+   url = "http://binghamton-asi.esuds.net/RoomStatus/machineStatus.i?bottomLocationId=#{id}"
+   page = Nokogiri::HTML(open(url))
+   machines = page.css('font')
 
-    machines[0..(machines.length/2 - 1)].each do |machine|
-        if (machine.text == "Available" or machine.text == "Cycle Complete") then washerAvail+=1 end
-        #puts machine.text
-    end
+   machines.each do |machine|
+      if (machine.text == "Available" or machine.text == "Cycle Complete") then machinesAvail+=1 end
+      #puts machine.text
+   end
 
-    machines[(machines.length/2)..(machines.length)].each do |machine|
-        if (machine.text == "Available" or machine.text == "Cycle Complete") then dryerAvail+=1 end
-        #puts machine.text
-    end
+   puts "#{building}: #{machinesAvail}/#{machines.length} are available"
 
-    puts "Washers avail: #{washerAvail} / #{machines[0..(machines.length/2 - 1)].length}"
-    puts "Dryers avail: #{dryerAvail} / #{machines[(machines.length/2)..(machines.length)].length}"
-
-    CSV.open("log.csv", "ab") do |csv|
-       csv << [building, Time.new, washerAvail, dryerAvail, machines[0..(machines.length/2 - 1)].length]
-    end
+   CSV.open("log.csv", "ab") do |csv|
+      csv << [building, Time.new, machinesAvail, machines.length]
+   end
 end
-
-
 
 
