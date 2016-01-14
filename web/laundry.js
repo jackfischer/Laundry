44c30f0bd8f453b28389f2668353a86d2f93cea7
Jack Fischer <jfische8@binghamton.edu>
2016-01-14 13:04:54 -0500

    more formal set up, starting to come together

--- web/laundry.js
+++ web/laundry.js
@@ -1,53 +1,28 @@
-'use strict'
-
-
-var data = {};
-var option = {};
+//'use strict'
 
 var ctx = document.getElementById("myChart");
+
 var linechart = new Chart(ctx, {
   type: 'line',
-  data: {
-    labels: ['mon', 'tues', 'weds'],
-    datasets: [{
-      label: "washer",
-      data: [.3, .7, .5]
-    },
-    {
-      label: "dryers",
-      data: [.4, .5, .2]
-    }]
-  },
+  data: daily_data,
   options: {
-    global: {
-      responsiveAnimationDuration: 2.0
-    }
+    responsive: false
   }
 });
 
 
 
 
+document.getElementById("weekly").onclick=function() {
+  console.log("weekly");
+  linechart.data.labels = weekly_data.labels;
+  linechart.data.datasets = weekly_data.datasets;
+  linechart.update();
+};
 
-
-//var ctx = document.getElementById("myChart");
-//var myChart = new Chart(ctx, {
-//  type: 'bar',
-//  data: {
-//    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//    datasets: [{
-//      label: '# of Votes',
-//      data: [12, 19, 3, 5, 2, 3]
-//    }]
-//  },
-//  options:{
-//    scales:{
-//      yAxes:[{
-//        ticks:{
-//          beginAtZero:true
-//        }
-//      }]
-//    }
-//  }
-//});
-//
+document.getElementById("daily").onclick=function() {
+  console.log("daily");
+  linechart.data.labels = daily_data.labels;
+  linechart.data.datasets = daily_data.datasets;
+  linechart.update();
+};
