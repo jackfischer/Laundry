d1ad1461564799b80e1c18479659a6b36c5eae77
Jack Fischer <jfische8@binghamton.edu>
2016-01-16 16:29:48 -0500

    visuals more or less down

--- web/laundry.js
+++ web/laundry.js
@@ -5,7 +5,7 @@ var ctx = document.getElementById("myChart");
 var linechart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels:["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    labels:["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
     datasets: [{
       label: "Washers",
       data: [5,5,6,5,2,7,5],
@@ -19,7 +19,7 @@ var linechart = new Chart(ctx, {
   },
 
   options: {
-    responsive: true
+    responsive: false
   }
 });
 
@@ -27,14 +27,16 @@ var linechart = new Chart(ctx, {
 
 
 document.getElementById("weekly").onclick=function() {
-  linechart.data.labels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  linechart.data.labels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   linechart.data.datasets = [{
     label: "Washers",
-    data: [5,5,6,5,2,7,5]
+    data: [5,5,6,5,2,7,5],
+    fill:false
   },
   {
     label: "Dryers",
-    data: [4,4,5,4,1,6,4]
+    data: [4,4,5,4,1,6,4],
+    fill:false
   }];
 
   linechart.update();
@@ -45,11 +47,13 @@ document.getElementById("daily").onclick=function() {
   linechart.data.labels = [1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12];
   linechart.data.datasets = [{
     label: "Washers",
-    data:[2,6,7,3,2,5,5,6,5,2,7,5,5,5,6,5,2,7,5,6,8,2,2,9]
+    data:[2,6,7,3,2,5,5,6,5,2,7,5,5,5,6,5,2,7,5,6,8,2,2,9],
+    fill:false
   },
   {
     label: "Dryers",
-    data: [2,6,2,3,2,2,5,6,5,4,7,5,4,5,6,5,2,7,5,6,8,2,2,9]
+    data: [2,6,2,3,2,2,5,6,5,4,7,5,4,5,6,5,2,7,5,6,8,2,2,9],
+    fill:false
   }];
 
   linechart.update();
